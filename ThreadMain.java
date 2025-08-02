e3151520e0a3bcbade35a51e8cf76bb8689c26a5
Harathi Devireddy <devireddyharathi@gmail.com>
2025-08-02 11:18:44 +0530

    Add files via upload

---
+++ ThreadMain.java
@@ -0,0 +1,13 @@
+package typesofvariables;
+
+
+	
+	public class ThreadMain {
+		public static void main(String[] args) {
+	      ThreadDemo t1=new ThreadDemo();
+	     
+	     // ThreadDemo t2=new ThreadDemo();
+	      t1.start();
+			//t2.start();	
+		}
+	}
