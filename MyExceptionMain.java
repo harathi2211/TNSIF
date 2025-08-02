a6fa539d42d8028359807f1ef1ea2c55a6367201
Harathi Devireddy <devireddyharathi@gmail.com>
2025-08-02 11:17:01 +0530

    Add files via upload

---
+++ MyExceptionMain.java
@@ -0,0 +1,11 @@
+package typesofvariables;
+
+
+	
+public class MyExceptionMain {
+	public static void main(String[] args) throws ArithmeticException, MyException {
+     ValidateAge.validateAge(12);	
+	}
+}
+
+
