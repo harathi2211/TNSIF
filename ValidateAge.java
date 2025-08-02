515a9f9c6e243b6f078233903907ae6f67857f53
Harathi Devireddy <devireddyharathi@gmail.com>
2025-08-02 11:20:44 +0530

    Add files via upload

---
+++ ValidateAge.java
@@ -0,0 +1,17 @@
+package typesofvariables;
+
+public class ValidateAge {
+	
+	
+	public static void validateAge(int age) throws MyException,ArithmeticException {
+		if(age<0) {
+			throw new MyException("The entered age is not valid");
+		}
+		else {
+			System.out.println("The age is valid");
+		}
+	}
+	
+}
+
+
